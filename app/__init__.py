4256673bcf0a9c34062f8f7ef6983fa5c40f50a3
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-10-26 15:20:07 +0300

    ADDS THE CONTROL FLOW FOR THE HEAD SECTION OF THE INDEX.HTML FILE AND THE MOVIE.HTML FILE, MOVES THE DEBUG = TRUE ARGUMENT TO THE CONFIG.PY FILE IN THE APP FOLDER, AND CREATES A NEW DIRECTORY CALLED INSTANCE FOR STORING THE API KEY

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .config import DevConfig
 
 # Initializing the application
 app = Flask(__name__)
+
+# setting up the configuration
+app.config.from_object(DevConfig)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -2,9 +2,19 @@ from flask import render_template
 from app import app
 
 # Views
+
 @app.route('/')
 def index():
     '''
     View root page functions that returns the index page and its data
     '''
-    return render_template('index.html')
+    
+    title = "Home - welcome to the best Movie Review Website Online"
+    return render_template('index.html', title=title)
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    '''
+    view movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html', id=movie_id)
