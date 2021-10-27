24433807400e83c4b40b47b03ce03b397b8a8915
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-10-27 13:37:37 +0300

    UPDATES THE MOVIE.HTML FILE, THE MACROS.HTML FILE, AND THE INDEX.HTML FILE

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .request import get_movies, get_movie
+
 
 # Views
 
@@ -18,9 +19,11 @@ def index():
     return render_template('index.html', title=title, popular = popular_movies, upcoming = upcoming_movies, now_showing = now_showing_movies)
 
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+@app.route('/movie/<int:id>')
+def movie(id):
     '''
     view movie page function that returns the movie details page and its data
     '''
-    return render_template('movie.html', id=movie_id)
+    movie = get_movie(id)
+    title = f'{movie.title}'
+    return render_template('movie.html', title=title, movie=movie)
